90a63d2e0edb9b78c78fcbd041491b2eb1733d6b
Benedict <benaq9@gmail.com>
2018-09-20 13:30:30 -0400

    moved functions from data_pipeline.py to utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -3,6 +3,29 @@ import numpy as np
 
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from scipy.stats import levene
+from sklearn.model_selection import train_test_split
+
+def train_test_split_to_csv(filename, random_state):
+    # Load dataset
+    df = pd.read_csv(filename, index_col=0)
+
+    # Train test split
+    train, test = train_test_split(df, test_size = 0.25, random_state=random_state)
+
+    # Save to csv
+    train.to_csv('data/train.csv')
+    test.to_csv('data/test.csv')
+
+def data_clean(df):
+    df['lnprice'] = np.log(df['price'])
+    df['carat2'] = df['carat'] ** 2
+
+    df['recolor'] = df['color']
+
+    df['recolor'].replace(to_replace='D', value='DE', inplace=True)
+    df['recolor'].replace(to_replace='E', value='DE', inplace=True)
+    df['recolor'].replace(to_replace='F', value='FG', inplace=True)
+    df['recolor'].replace(to_replace='G', value='FG', inplace=True)
 
 def VIF(df):
     '''
